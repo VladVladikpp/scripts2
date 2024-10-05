e7e95bf3a55f5437bac382a9ef933a8ad4491f41
VladVladikpp <165013712+VladVladikpp@users.noreply.github.com>
2024-10-05 11:54:34 +0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -6,3 +6,10 @@ local Tab = Window:NewTab("Основное")
 
 local Tab2 = Window:NewTab("Wall Hack")
 local Section2 = Tab:NewSection("Wall Hack")
+Section2:NewToggle("Wall Hack", "Видимость игроков через стены", function(state)
+    if state then
+        print("Toggle On")
+    else
+        print("Toggle Off")
+    end
+end)
